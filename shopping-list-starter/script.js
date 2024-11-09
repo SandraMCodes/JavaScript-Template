c2ae7a3c6e2a3151eff56d48dab37d7e65d31afa
SandraMCodes <sandram97@hotmail.com>
2024-11-09 12:34:11 +0800

    Did shopping list and counter

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,7 +1,37 @@
-const addItem = () => {
+let shoppingListItems = ["milk", "eggs", "bread"];
+const listElement = document.getElementById("shopping-list-items");
+const newItemText = document.getElementById("new-item-text")
 
+
+const addItem = () => {
+let item = document.getElementById("new-item-text").value;
+shoppingListItems = [...shoppingListItems, item];
+updateItems();
 };
+
+const clearInput = () => {
+newItemText.value = ""
+}
 
 const updateItems = () => {
+// First we get the list element
+ 
+ // Then we clear it of any existing items
+ listElement.innerHTML = "";
 
-};
+ // Then we loop through the shopping list items and add them to the list
+ for (const shoppingItem of shoppingListItems) {
+   let itemElement = document.createElement("li");
+   itemElement.innerText = shoppingItem;
+   listElement.appendChild(itemElement);
+ }
+ 
+clearInput();
+
+}
+
+const clearItems = () => {
+    shoppingListItems=[]
+    updateItems();
+}
+updateItems()
